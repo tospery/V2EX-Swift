b595d473bd22d4145ba61bf62890104c1c4a57aa
yangjianxiang <tospery@gmail.com>
2020-11-28 02:45:25 +0800

    工程模板

---
+++ V2EX/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  V2EX
+//
+//  Created by 杨建祥 on 2020/11/28.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ V2EX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  V2EX
+//
+//  Created by 杨建祥 on 2020/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
