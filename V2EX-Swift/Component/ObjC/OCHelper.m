bc1e80b52db698dd76178516461ea99f77d9e5a1
yangjianxiang <yangjianxiang@xunyou.com>
2021-04-14 17:57:46 +0800

    更新SWFrame

--- V2EX-Swift/Component/ObjC/OCHelper.m
+++ V2EX-Swift/Component/ObjC/OCHelper.m
@@ -9,4 +9,13 @@
 
 @implementation OCHelper
 
++ (instancetype)sharedInstance {
+    static id instance;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[[self class] alloc] init];
+    });
+    return instance;
+}
+
 @end
